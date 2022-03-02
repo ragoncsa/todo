87c2e41376dd5638a77c1111c578f199ce480155
Tamas Ragoncsa <ragoncsa@gmail.com>
2022-03-02 06:39:48 +0200

    added openapi spec to the rest of the services

--- http/taskservice.go
+++ http/taskservice.go
@@ -20,14 +20,14 @@ type TaskService struct {
 // @BasePath /
 
 // GetTasks godoc
-// @Summary Get all tasks
+// @Summary       Get all tasks
 // @Schemes
-// @Description Reads and returns all the tasks.
-// @Tags read
-// @Accept json
-// @Produce json
-// @Success 200
-// @Router /tasks/ [get]
+// @Description   Reads and returns all the tasks.
+// @Tags          read
+// @Accept        json
+// @Produce       json
+// @Success       200
+// @Router        /tasks/ [get]
 func (t *TaskService) GetTasks(c *gin.Context) {
 	tasks, err := t.Service.Tasks()
 	if err != nil {
@@ -37,6 +37,16 @@ func (t *TaskService) GetTasks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, tasks)
 }
 
+// GetTasks godoc
+// @Summary       Get task
+// @Schemes
+// @Description   Reads a single task and returns it.
+// @Tags          read
+// @Accept        json
+// @Produce       json
+// @Param         taskid  path  int  true  "Task ID"
+// @Success       200
+// @Router        /tasks/{taskid} [get]
 func (t *TaskService) GetTask(c *gin.Context) {
 	id := c.Param("taskid")
 
@@ -53,6 +63,16 @@ func (t *TaskService) GetTask(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, task)
 }
 
+// CreateTask godoc
+// @Summary       Creates task
+// @Schemes
+// @Description   Creates a new task.
+// @Tags          write
+// @Accept        json
+// @Produce       json
+// @Param         task  body      domain.Task  true  "New task"
+// @Success       200
+// @Router        /tasks/ [post]
 func (t *TaskService) CreateTask(c *gin.Context) {
 	var request CreateTaskRequest
 	if err := c.ShouldBindJSON(&request); err != nil {
@@ -64,6 +84,16 @@ func (t *TaskService) CreateTask(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, task)
 }
 
+// DeleteTasks godoc
+// @Summary       Deletes task
+// @Schemes
+// @Description   Deletes a single task.
+// @Tags          write
+// @Accept        json
+// @Produce       json
+// @Param         taskid  path  int  true  "Task ID"
+// @Success       200
+// @Router        /tasks/{taskid} [delete]
 func (t *TaskService) DeleteTask(c *gin.Context) {
 	id := c.Param("taskid")
 
@@ -80,6 +110,15 @@ func (t *TaskService) DeleteTask(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, struct{}{})
 }
 
+// GetTasks godoc
+// @Summary       Delete all tasks
+// @Schemes
+// @Description   Deletes all the tasks.
+// @Tags          write
+// @Accept        json
+// @Produce       json
+// @Success       200
+// @Router        /tasks/ [delete]
 func (t *TaskService) DeleteTasks(c *gin.Context) {
 	err := t.Service.DeleteTasks()
 	if err != nil {
