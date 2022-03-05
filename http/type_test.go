b0cf66f8c0331d87e51d16cb0d61e0050e946ee9
Tamas Ragoncsa <ragoncsa@gmail.com>
2022-03-05 20:12:05 +0200

    improve tests of timestamp conversion

--- http/type_test.go
+++ http/type_test.go
@@ -6,29 +6,43 @@ import (
 )
 
 func TestTimestampTimeToJSON(t *testing.T) {
-	s := "2019-10-12T07:20:50.52Z"
+	const timeVal = "2019-10-12T07:20:50Z"
+	time, err := time.Parse(time.RFC3339, timeVal)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	ttt := &TimestampTime{
+		Time: time,
+	}
+
+	bin, err := ttt.MarshalJSON()
+	if err != nil {
+		t.Fatalf("MarshalJSON failed: %v", err)
+	}
+
+	if got, want := string(bin), "\""+timeVal+"\""; got != want {
+		t.Fatalf("got %v, want %v", got, want)
+	}
+
+}
+
+func TestTimestampTimeFromJSON(t *testing.T) {
+	const timeVal = "2019-10-12T07:20:50Z"
+	const jsonTimeVal = "\"" + timeVal + "\""
 	ttt := &TimestampTime{}
-	bin := make([]byte, len(s))
-	bin = append(bin, s...)
+	bin := make([]byte, len(jsonTimeVal))
+	bin = append(bin, []byte(jsonTimeVal)...)
 
 	err := ttt.UnmarshalJSON(bin)
 	if err != nil {
 		t.Fatalf("UnmarshalJSON failed: %v", err)
 	}
 
-	want, err := time.Parse(time.RFC3339, s)
+	want, err := time.Parse(time.RFC3339, timeVal)
 	if err != nil {
-		t.Fatalf("UnmarshalJSON failed: %v", err)
+		t.Fatalf("unexpected error: %v", err)
 	}
 	if got := ttt; !got.Equal(want) {
 		t.Fatalf("got %v, want %v", got, want)
 	}
-}
-
-func TestTimestampTimeFromJSON(t *testing.T) {
-
-}
-
-func TestTaskMapping(t *testing.T) {
-
 }
